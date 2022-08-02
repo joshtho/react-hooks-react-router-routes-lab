b6433f916b747bbc46d2a0ede3574401d6c72a7e
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-08-01 19:30:25 -0500

    checking for movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title} >
+          Title: {movie.title + " "}
+          Time: {" " + movie.time + "min"}
+          <ul>
+            Genres: {movie.genres.map((genre) => (
+              <div key={genre}>
+                <li>{genre}</li>
+              </div>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  ) 
 }
 
 export default Movies;
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,23 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact style={{margin: "0 6px 6px"}} activeStyle={{background: "yellow"}} >
+        Home
+      </NavLink>
+      <NavLink to="/movies" exact style={{margin: "0 6px 6px"}} activeStyle={{background: "yellow"}} >
+        Movies
+      </NavLink>
+      <NavLink to="/directors" exact style={{margin: "0 6px 6px"}} activeStyle={{background: "yellow"}} >
+        Directors
+      </NavLink>
+      <NavLink to="/actors" exact style={{margin: "0 6px 6px"}} activeStyle={{background: "yellow"}} >
+        Actors
+      </NavLink>
+    </div>
+
+  ) 
 }
 
 export default NavBar;
